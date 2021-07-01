70133c7d40c1d924855e045060409855e070d3ee
Will Cohen <chewnoill@gmail.com>
2021-07-01 16:51:11 -0400

    [chore] add stub websocket service

--- src/websocket-service.ts
+++ src/websocket-service.ts
@@ -1,5 +1,8 @@
-/** 
- * 
+import ws from 'ws'
+import http from 'http'
+
+/**
+ *
  * [`y-websocket`](https://github.com/yjs/y-websocket) provides a template
  * websocket services to interface with their websocket provider
  * https://github.com/yjs/y-websocket/blob/master/bin/server.js
@@ -22,3 +25,31 @@
  */
  
 
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 6001;
+// @ts-ignore
+const wss = new ws.Server({ noServer: true });
+
+const server = http.createServer((_, response) => {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("okay");
+});
+
+wss.on("connection", function(){
+  console.log("made it here!");
+});
+
+server.on("upgrade", (request, socket, head) => {
+  // You may check auth of request here..
+  /**
+   * @param {any} ws
+   */
+  const handleAuth = (ws) => {
+    wss.emit("connection", ws, request);
+  };
+  wss.handleUpgrade(request, socket, head, handleAuth);
+});
+
+server.listen({ host, port });
+
+console.log("Signaling server running on", host, ":", port);
