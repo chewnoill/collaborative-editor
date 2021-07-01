1cb8d44c6dc3f52a7ea2c8ad5e5937a7f21ff388
Will Cohen <chewnoill@gmail.com>
2021-07-01 17:29:42 -0400

    [feat] setup websocket provider for the client
    and connect it to the same room and port as
    the webrtc provider

--- src/editor.tsx
+++ src/editor.tsx
@@ -5,16 +5,24 @@ import { WebrtcProvider } from "y-webrtc";
 import CodeMirror from "codemirror";
 import "codemirror/mode/markdown/markdown.js";
 import "codemirror/lib/codemirror.css";
+import { WebsocketProvider } from "y-websocket";
 
-const SIGNALLING_SERVICE = process.env.STORYBOOK_SIGNAL_URL;
+const ROOM_NAME = "new-room";
+const SIGNALLING_SERVICE = process.env.STORYBOOK_SIGNAL_URL|| 'ws://localhost:6001';
 
 export default function Editor() {
   const ref = React.useRef();
   React.useEffect(() => {
     const ydoc = new Y.Doc();
-    const provider = new WebrtcProvider("new-room", ydoc, { signaling: [SIGNALLING_SERVICE] } as any);
+    const provider = new WebrtcProvider(ROOM_NAME, ydoc, { signaling: [SIGNALLING_SERVICE] } as any);
     const yText = ydoc.getText("codemirror");
     const yUndoManager = new Y.UndoManager(yText);
+
+    const wsProvider = new WebsocketProvider(SIGNALLING_SERVICE, ROOM_NAME, ydoc)
+
+    wsProvider.on('status', event => {
+      console.log(event.status) // logs "connected" or "disconnected"
+    })
     
     const editor = CodeMirror(ref.current, {
       mode: "markdown",
