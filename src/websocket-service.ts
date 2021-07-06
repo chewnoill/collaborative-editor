dcc70423a2ee86f96d252a4ed154e4d813ffa90a
Will Cohen <chewnoill@gmail.com>
2021-07-06 15:29:05 -0400

    [chore] cleanup old comments

--- src/websocket-service.ts
+++ src/websocket-service.ts
@@ -2,33 +2,8 @@ import ws from 'ws'
 import http from 'http'
 import setupWSConnection from './socket-message-handling';
 
-/**
- *
- * [`y-websocket`](https://github.com/yjs/y-websocket) provides a template
- * websocket services to interface with their websocket provider
- * https://github.com/yjs/y-websocket/blob/master/bin/server.js
- * 
- * [`y-webrtc`](https://github.com/yjs/y-webrtc) provides
- * a thin signalling backing service that connects to their
- * webrtc client provider https://github.com/yjs/y-webrtc/blob/master/bin/server.js 
- *
- * TODO: Build this. 
- * 
- * Functions from both services should be combined here, to produce
- * a single websocket connection that provides both websocket
- * push changes, and initial document loads, as well as connecting
- * peers to each other.
- * 
- * Data persistance.
- *   The y-websocket interface implements a leveldb data layer interface.
- *   * What is leveldb?
- *   * Implement connector to some docker database.
- */
- 
-
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 6001;
-// @ts-ignore
 const wss = new ws.Server({ noServer: true });
 
 const server = http.createServer((_, response) => {
@@ -39,10 +14,6 @@ const server = http.createServer((_, response) => {
 wss.on("connection", setupWSConnection);
 
 server.on("upgrade", (request, socket, head) => {
-  // You may check auth of request here..
-  /**
-   * @param {any} ws
-   */
   const handleAuth = (ws) => {
     wss.emit("connection", ws, request);
   };
