a62449157bf77f054d645eb00735170490420a8b
Will Cohen <chewnoill@gmail.com>
2021-07-07 15:51:58 -0400

    [feat] add passport auth

--- src/websocket-service.ts
+++ src/websocket-service.ts
@@ -1,16 +1,52 @@
+import express from 'express'
+import passport from 'passport'
+import LocalStrategy from 'passport-local'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import session from 'express-session'
 import ws from 'ws'
 import http from 'http'
 import setupWSConnection from './socket-message-handling';
 
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 6001;
-const wss = new ws.Server({ noServer: true });
+const SESSION_SECRET = process.env.SESSION_SECRET || 'big secret'
 
-const server = http.createServer((_, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("okay");
+const app = express();
+
+passport.use(new LocalStrategy(
+  function (username, _password, done) {
+    return done(null, { username });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  done(null, JSON.stringify(user));
 });
 
+passport.deserializeUser(function (data, done) {
+  const user = JSON.parse(data);
+  done(null, user);
+});
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: 'auto' }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/' })
+);
+
+const server = http.createServer(app);
+
+const wss = new ws.Server({ noServer: true });
 wss.on("connection", setupWSConnection);
 
 server.on("upgrade", (request, socket, head) => {
