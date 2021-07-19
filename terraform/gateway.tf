934632484319679b62fee0f3fd3d18b314a2a5fb
Will Cohen <chewnoill@gmail.com>
2021-07-19 10:54:07 -0400

    [terraform] provision a gateway running nginx
    and connect it to our external ip

---
+++ terraform/gateway.tf
@@ -0,0 +1,50 @@
+
+resource "google_compute_instance" "gateway" {
+  project = var.project_name
+  name         = "gateway"
+  machine_type = "f1-micro"
+  zone = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.default.name
+    access_config {
+      nat_ip = google_compute_address.gateway-address.address
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+    echo "*****    Installing Nginx    *****"
+    apt update
+    apt install -y nginx
+    echo "*****    Configure Nginx    *****"
+    echo '${data.template_file.nginx_config.rendered}' > /etc/nginx/conf.d/default.conf
+    rm -rf /etc/nginx/sites-enabled
+    
+    echo "*****    Start Nginx    *****"
+    systemctl enable nginx
+    systemctl restart nginx
+    echo "*****   Startup script completes!!    *****"
+    EOF
+
+  service_account {
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+data "template_file" "nginx_config" {
+  template = file("nginx/default.conf.template")
+  vars = {
+    PORT = 80
+    STATIC_URL = "http://storage.googleapis.com/${var.public_gcs_bucket_name}/client"
+    WEBSOCKET_URL = "http://${google_compute_instance.app.network_interface.0.network_ip}:6001"
+    API_URL = "http://${google_compute_instance.app.network_interface.0.network_ip}:6001"
+  }
+}
