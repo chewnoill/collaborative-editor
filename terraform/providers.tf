04e55104512590ae52c2e2ed0395dd1182293a11
Will Cohen <chewnoill@gmail.com>
2021-07-19 10:46:24 -0400

    [terraform] add terraform providers
    there is a manual step here that is a little
    annoying.
    
    A GCS bucket needs to be manually created before
    using the terraform provider. The bucket is used
    to store terraform state. Should probably not be
    public, although it shouldn't contain sensative
    information either.

---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+  backend "gcs" {
+    # var.public_gcs_bucket_name
+    bucket  = "willdocs-1"
+    prefix  = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.project_name
+  zone = "us-east1-b"
+}
+
