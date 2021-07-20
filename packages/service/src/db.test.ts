63e1a86aa47ee44c7b75b6659f204e5b1e4bbbde
tucker-weed <tuckerjweed@gmail.com>
2021-07-20 14:32:03 -0400

    [feat] database functionality tests

--- packages/service/src/db.test.ts
+++ packages/service/src/db.test.ts
@@ -1,6 +1,29 @@
 import * as Y from "yjs";
+import db from "./utils/db";
 
-test("crdts can be persisted", () => {
+afterAll(() => {
+  db.$disconnect();
+});
+
+test("user can be created", async () => {
+  // create a user
+  const user = await db.users.create({
+    data: {
+      name: "test user",
+    },
+  });
+  expect(user.name).toBe("test user");
+
+  // read the created user entry
+  const sameUser = await db.users.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+  expect(user).toEqual(sameUser);
+});
+
+test("crdts can be persisted", async () => {
   // create a yjs document A
   const ydoc = new Y.Doc();
   // make a change to the document
@@ -9,10 +32,22 @@ test("crdts can be persisted", () => {
   const state = Y.encodeStateAsUpdate(ydoc);
 
   // persist this change to the database
-  // ????
+  const dbDoc = await db.document.create({
+    data: {
+      value: "test value",
+      origin: Buffer.from(state),
+      web_rtc_key: "",
+    },
+  });
 
   // get the change back from the database
-  const stateFromDatabase = state;
+  const stateFromDatabase = (
+    await db.document.findFirst({
+      where: {
+        id: dbDoc.id,
+      },
+    })
+  ).origin;
 
   // create yjs document B from database change
   const ydoc2 = new Y.Doc();
