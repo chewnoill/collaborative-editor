2ae883db474df191d85596c3766076c3df1eaee4
Will Cohen <chewnoill@gmail.com>
2021-07-21 08:41:52 -0400

    [chore] add test outlines

---
+++ packages/service/src/utils/we-shared-doc.test.ts
@@ -0,0 +1,16 @@
+describe("WSSharedDoc", () => {
+  test("can be fetched from the data store", () => {
+    // 1. setup: create document in the data store
+    // 2. fetch a yjs document
+    // 3. expect documents to match
+  });
+
+  test("can persist updates to the document", () => {
+    // 1. setup: create document in the data store
+    // 2. fetch a yjs document
+    // 3. create change and send it to the yjs document
+    // 4. expect change to be persisted
+  });
+
+  test("can recieve changes in any order, will produce the same results", () => { });
+});
