7c36c53c4bb418ed4a07b8db14ba7b7e61516223
Will Cohen <chewnoill@gmail.com>
2021-07-23 10:04:07 -0400

    [chore] fill in db test

--- packages/service/src/db.test.ts
+++ packages/service/src/db.test.ts
@@ -1,26 +1,25 @@
 import * as Y from "yjs";
+import { pool, db } from "./db";
+import { createDocument, insertUpdate } from "./utils/documents";
+import { getYDoc } from "./utils/ws-shared-doc";
 
-test("crdts can be persisted", () => {
+afterAll(() => {
+  return pool.end();
+});
+
+test("crdts can be persisted", async () => {
   // create a yjs document A
   const ydoc = new Y.Doc();
-  // make a change to the document
   const ytext = ydoc.getText("codemirror");
   ytext.insert(0, "hello world");
-  const state = Y.encodeStateAsUpdate(ydoc);
-
+  const { id, origin } = await createDocument(ydoc);
+  // make a change to the document
+  ytext.insert(0, "some new text");
   // persist this change to the database
-  // ????
-
+  await insertUpdate(id, Y.encodeStateAsUpdate(ydoc, db.toBuffer(origin)));
   // get the change back from the database
-  const stateFromDatabase = state;
-
-  // create yjs document B from database change
-  const ydoc2 = new Y.Doc();
-  Y.applyUpdate(ydoc2, stateFromDatabase);
+  const ydocUpdate = await getYDoc(id);
 
   // expect that the contents of document A matches document B
-  // test the result
-  expect(ydoc.getText("codemirror").toJSON()).toBe(
-    ydoc2.getText("codemirror").toJSON()
-  );
+  expect(ytext.toJSON()).toMatch(ydocUpdate.getText("codemirror").toJSON());
 });
