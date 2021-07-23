93e3aab7f3130ac1df765b3a14e2233fbd69b384
Will Cohen <chewnoill@gmail.com>
2021-07-23 10:01:57 -0400

    [fix] jest config

--- packages/service/jest.config.js
+++ packages/service/jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.js?$": "babel-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
