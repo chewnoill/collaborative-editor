0e72488848d325c8702daf73eb69f15dd79e4266
Will Cohen <chewnoill@gmail.com>
2021-07-25 22:52:55 -0400

    [chore] switch to webpack
    rollup wasn't working, switching to webpack

---
+++ packages/service/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const NodemonPlugin = require("nodemon-webpack-plugin");
+const {DefinePlugin} = require("webpack");
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  entry: {
+    bundle: "./src/service.ts",
+  },
+  optimization: {
+    minimize: false,
+    minimizer: [new TerserPlugin()],
+  },
+  devtool: "cheap-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      }
+    ],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".mjs", ".ts", ".tsx", ".js", ".jsx", ".md"],
+    plugins: [],
+  },
+  plugins: [
+    new NodemonPlugin(),
+    new DefinePlugin({ "global.GENTLY": false }),
+  ],
+  node: {
+    __dirname: true
+  }
+};
