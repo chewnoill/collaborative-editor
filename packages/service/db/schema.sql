a6bc9c3c3cc3e8c0b7253b5abe9a1904fbe025be
Will Cohen <chewnoill@gmail.com>
2021-07-27 09:27:10 -0400

    [chore] postgraphile is adding some functions
    looks like it wants to get notified about schema changes.
    Not sure where this is coming from or if I can disable.

--- packages/service/db/schema.sql
+++ packages/service/db/schema.sql
@@ -10,6 +10,13 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: postgraphile_watch; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA postgraphile_watch;
+
+
+--
 -- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -38,6 +45,86 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UU
 
 
 --
+-- Name: notify_watchers_ddl(); Type: FUNCTION; Schema: postgraphile_watch; Owner: -
+--
+
+CREATE FUNCTION postgraphile_watch.notify_watchers_ddl() RETURNS event_trigger
+    LANGUAGE plpgsql
+    AS $$
+begin
+  perform pg_notify(
+    'postgraphile_watch',
+    json_build_object(
+      'type',
+      'ddl',
+      'payload',
+      (select json_agg(json_build_object('schema', schema_name, 'command', command_tag)) from pg_event_trigger_ddl_commands() as x)
+    )::text
+  );
+end;
+$$;
+
+
+--
+-- Name: notify_watchers_drop(); Type: FUNCTION; Schema: postgraphile_watch; Owner: -
+--
+
+CREATE FUNCTION postgraphile_watch.notify_watchers_drop() RETURNS event_trigger
+    LANGUAGE plpgsql
+    AS $$
+begin
+  perform pg_notify(
+    'postgraphile_watch',
+    json_build_object(
+      'type',
+      'drop',
+      'payload',
+      (select json_agg(distinct x.schema_name) from pg_event_trigger_dropped_objects() as x)
+    )::text
+  );
+end;
+$$;
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.users (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    name text NOT NULL,
+    password text DEFAULT ''::text NOT NULL
+);
+
+
+--
+-- Name: TABLE users; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE public.users IS '
+@name users
+@omit create,update,delete';
+
+
+--
+-- Name: create_user(text); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.create_user(name text) RETURNS public.users
+    LANGUAGE sql STRICT
+    AS $$
+  INSERT INTO users
+    (name)
+    VALUES (name)
+    RETURNING *;
+$$;
+
+
+--
 -- Name: current_user_id(); Type: FUNCTION; Schema: public; Owner: -
 --
 
@@ -48,9 +135,50 @@ CREATE FUNCTION public.current_user_id() RETURNS uuid
 $$;
 
 
-SET default_tablespace = '';
+--
+-- Name: user_document; Type: TABLE; Schema: public; Owner: -
+--
 
-SET default_table_access_method = heap;
+CREATE TABLE public.user_document (
+    document_id uuid,
+    user_id uuid,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL
+);
+
+
+--
+-- Name: TABLE user_document; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE public.user_document IS '
+@name user_document
+@omit create,update,delete';
+
+
+--
+-- Name: invite_user_to_document(text, uuid); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.invite_user_to_document(name text, document_id uuid) RETURNS public.user_document
+    LANGUAGE sql STRICT
+    AS $$
+  INSERT INTO user_document
+    (user_id, document_id)
+    VALUES ((SELECT users.id FROM users where name=name), document_id)
+    RETURNING *;
+$$;
+
+
+--
+-- Name: me(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.me() RETURNS public.users
+    LANGUAGE sql STABLE
+    AS $$
+  SELECT * FROM users WHERE id = current_user_id()
+$$;
+
 
 --
 -- Name: document; Type: TABLE; Schema: public; Owner: -
@@ -67,6 +195,24 @@ CREATE TABLE public.document (
 
 
 --
+-- Name: TABLE document; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE public.document IS '
+@name document
+@omit create,update,delete';
+
+
+--
+-- Name: COLUMN document.latest_update_time; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON COLUMN public.document.latest_update_time IS '
+@name latest_update_time
+@omit create,update';
+
+
+--
 -- Name: document_updates_queue; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -79,6 +225,15 @@ CREATE TABLE public.document_updates_queue (
 
 
 --
+-- Name: TABLE document_updates_queue; Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON TABLE public.document_updates_queue IS '
+@name document_updates_queue
+@omit';
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -88,25 +243,12 @@ CREATE TABLE public.schema_migrations (
 
 
 --
--- Name: user_document; Type: TABLE; Schema: public; Owner: -
+-- Name: TABLE schema_migrations; Type: COMMENT; Schema: public; Owner: -
 --
 
-CREATE TABLE public.user_document (
-    document_id uuid,
-    user_id uuid,
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL
-);
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.users (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    name text NOT NULL,
-    password text DEFAULT ''::text NOT NULL
-);
+COMMENT ON TABLE public.schema_migrations IS '
+@name schema_migrations
+@omit';
 
 
 --
@@ -198,10 +340,26 @@ ALTER TABLE ONLY public.user_document
 
 
 --
+-- Name: document create_document_for_current_user; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY create_document_for_current_user ON public.document FOR INSERT TO postgraphile_user WITH CHECK ((creator_id = public.current_user_id()));
+
+
+--
 -- Name: document; Type: ROW SECURITY; Schema: public; Owner: -
 --
 
 ALTER TABLE public.document ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: user_document invite_to_document_if_allowed; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY invite_to_document_if_allowed ON public.user_document FOR INSERT TO postgraphile_user WITH CHECK ((document_id IN ( SELECT document.id
+   FROM public.document
+  WHERE (document.creator_id = public.current_user_id()))));
+
 
 --
 -- Name: document select_document_if_allowed; Type: POLICY; Schema: public; Owner: -
@@ -210,6 +368,29 @@ ALTER TABLE public.document ENABLE ROW LEVEL SECURITY;
 CREATE POLICY select_document_if_allowed ON public.document FOR SELECT TO postgraphile_user USING (((creator_id = public.current_user_id()) OR (id IN ( SELECT user_document.document_id
    FROM public.user_document
   WHERE (user_document.user_id = public.current_user_id())))));
+
+
+--
+-- Name: user_document; Type: ROW SECURITY; Schema: public; Owner: -
+--
+
+ALTER TABLE public.user_document ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: postgraphile_watch_ddl; Type: EVENT TRIGGER; Schema: -; Owner: -
+--
+
+CREATE EVENT TRIGGER postgraphile_watch_ddl ON ddl_command_end
+         WHEN TAG IN ('ALTER AGGREGATE', 'ALTER DOMAIN', 'ALTER EXTENSION', 'ALTER FOREIGN TABLE', 'ALTER FUNCTION', 'ALTER POLICY', 'ALTER SCHEMA', 'ALTER TABLE', 'ALTER TYPE', 'ALTER VIEW', 'COMMENT', 'CREATE AGGREGATE', 'CREATE DOMAIN', 'CREATE EXTENSION', 'CREATE FOREIGN TABLE', 'CREATE FUNCTION', 'CREATE INDEX', 'CREATE POLICY', 'CREATE RULE', 'CREATE SCHEMA', 'CREATE TABLE', 'CREATE TABLE AS', 'CREATE VIEW', 'DROP AGGREGATE', 'DROP DOMAIN', 'DROP EXTENSION', 'DROP FOREIGN TABLE', 'DROP FUNCTION', 'DROP INDEX', 'DROP OWNED', 'DROP POLICY', 'DROP RULE', 'DROP SCHEMA', 'DROP TABLE', 'DROP TYPE', 'DROP VIEW', 'GRANT', 'REVOKE', 'SELECT INTO')
+   EXECUTE FUNCTION postgraphile_watch.notify_watchers_ddl();
+
+
+--
+-- Name: postgraphile_watch_drop; Type: EVENT TRIGGER; Schema: -; Owner: -
+--
+
+CREATE EVENT TRIGGER postgraphile_watch_drop ON sql_drop
+   EXECUTE FUNCTION postgraphile_watch.notify_watchers_drop();
 
 
 --
@@ -231,4 +412,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20210723153150'),
     ('20210726125556'),
     ('20210726224018'),
-    ('20210729151156');
+    ('20210729151156'),
+    ('20210802130820');
