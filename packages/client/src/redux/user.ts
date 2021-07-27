64b0897f4011d4a96e4ca43270c19ef17b40ad67
Will Cohen <chewnoill@gmail.com>
2021-07-27 11:35:32 -0400

    [chore] setup user slice

---
+++ packages/client/src/redux/user.ts
@@ -0,0 +1,27 @@
+import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
+
+interface UserState {
+  me?: { id: string; username: string };
+}
+
+// Define the initial state using that type
+const initialState: UserState = {};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, { payload }) => {
+      state.me = payload.user;
+    },
+  },
+});
+
+export const { login } = userSlice.actions;
+
+const selectUserSlice = (state: RootState) => state.user;
+
+export const selectUser = createSelector(selectUserSlice, (state) => state.me);
+
+export default userSlice.reducer;
