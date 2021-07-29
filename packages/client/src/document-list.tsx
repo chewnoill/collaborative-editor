b182f030714cc66bf74bb4ee6f508c588ac389fe
tucker-weed <tuckerjweed@gmail.com>
2021-07-29 10:57:45 -0400

    [feat] document interaction on client

--- packages/client/src/document-list.tsx
+++ packages/client/src/document-list.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setDocument,
+  setDocuments,
+  selectDocument,
+  selectUserDocuments,
+} from "./redux/document";
 
 const List = styled.div`
   max-width: 300px;
@@ -8,13 +15,29 @@ const List = styled.div`
 `;
 
 export default function DocumentList() {
-  const [documents, setDocuments] = React.useState(null);
+  const userDocs = useSelector(selectUserDocuments);
+  const doc = useSelector(selectDocument);
+  const dispatch = useDispatch();
   React.useEffect(() => {
-    fetch("/api/documents").then((data) => {
-      setDocuments(data.json());
-    });
+    fetch("/api/documents")
+      .then((data) => data.json())
+      .then((data) => dispatch(setDocuments(data.documents)));
   }, []);
-  if (!documents) return <div>loading...</div>;
+  if (!userDocs) return <div>loading...</div>;
 
-  return <List>{documents.length} documents have been found</List>;
+  return (
+    <div>
+      <p>{doc ? "Selected document" + " ID: " + doc.id : ""}</p>
+      <List>
+        {Object.keys(userDocs).length} documents have been found
+        {Object.values(userDocs).map((doc) => (
+          <li key={doc.id}>
+            <p>{"DOC ID: " + doc.id}</p>
+            <p>{"DOC OWNER " + doc.creator_id}</p>
+            <button onClick={() => dispatch(setDocument(doc))}>Select</button>
+          </li>
+        ))}
+      </List>
+    </div>
+  );
 }
