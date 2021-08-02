d224c65d43a800674f24ebf3131893e5a847dfe8
Will Cohen <chewnoill@gmail.com>
2021-08-02 10:41:56 -0400

    [chore] add gql client

--- packages/client/src/components/select-document.tsx
+++ packages/client/src/components/select-document.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { setDocument } from "../ducks/appState/document";
-import { useGetDocumentsQuery } from "../ducks/api";
+import { setDocument } from "ducks/appState/document";
+import { gql, useQuery } from "@apollo/client";
 
 export default function DocumentSelect() {
   const dispatch = useDispatch();
-  const { data, error, isLoading } = useGetDocumentsQuery();
-  if (error) {
-    console.log(error);
+
+  const { data, loading } = useQuery(gql`
+    query getDocumentList {
+      allDocuments {
+        edges {
+          node {
+            id
+            value
+          }
+        }
+      }
+    }
+  `);
+
+  if (loading) return <div>loading...</div>;
+  if (!data?.allDocuments?.edges) {
     return null;
   }
-  if (isLoading) return <div>loading...</div>;
 
   return (
     <select
@@ -21,7 +33,7 @@ export default function DocumentSelect() {
       <option id="" value="">
         select a document
       </option>
-      {data.map((doc) => (
+      {data.allDocuments.edges.map(({ node: doc }) => (
         <option key={doc.id} value={doc.id}>
           {doc.id.slice(0, 5)}
         </option>
