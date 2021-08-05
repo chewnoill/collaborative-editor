44f97969a6cd5977491fdaceebb3f1bfb4bd81f3
Will Cohen <chewnoill@gmail.com>
2021-08-05 17:16:11 -0400

    [refactor] rename user-form

--- packages/client/src/pages/login.tsx
+++ packages/client/src/pages/login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "components/login-form";
+import { LoginForm } from "components/user-form";
 
 export default function LoginPage() {
   return <LoginForm />;
