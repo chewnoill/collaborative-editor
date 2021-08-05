0b51f9ad75de61bb72a276fe141c06c635bf20a2
Will Cohen <chewnoill@gmail.com>
2021-08-05 17:26:46 -0400

    [chore] reuse the user form across other forms

--- packages/client/src/components/user-form.tsx
+++ packages/client/src/components/user-form.tsx
@@ -6,12 +6,29 @@ const Form = styled.form`
   flex-direction: column;
 `;
 
-export function LoginForm() {
+function UserForm({
+  children,
+  ...props
+}: React.FormHTMLAttributes<HTMLFormElement>) {
   return (
-    <Form action="/api/login" method="post">
+    <Form {...props}>
       <label>username</label> <input name="username" type="text" />
       <label>password</label> <input name="password" type="password" />
+      {children}
       <button type="submit">submit</button>
     </Form>
+  );
+}
+export function LoginForm() {
+  return <UserForm action="/api/login" method="post" />;
+}
+export function CreateUserForm() {
+  return <UserForm action="/api/users/create" method="post" />;
+}
+export function UpdatePasswordForm() {
+  return (
+    <UserForm action="/api/users/update-password" method="post">
+      <label>new password</label> <input name="new-password" type="password" />
+    </UserForm>
   );
 }
