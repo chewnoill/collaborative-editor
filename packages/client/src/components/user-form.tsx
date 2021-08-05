71b81940724b371119e58f031a9bfa0e3b9a178b
Will Cohen <chewnoill@gmail.com>
2021-08-05 17:16:47 -0400

    [chore] remove unused component

--- packages/client/src/components/user-form.tsx
+++ packages/client/src/components/user-form.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { selectUser } from "ducks/appState/user";
-import { useSelector } from "react-redux";
-import Me from "./me";
 
 const Form = styled.form`
   display: flex;
@@ -16,16 +13,5 @@ export function LoginForm() {
       <label>password</label> <input name="password" type="password" />
       <button type="submit">submit</button>
     </Form>
-  );
-}
-
-export function GuardedLoginForm() {
-  const me = useSelector(selectUser);
-  console.log(me);
-
-  return (
-    <>
-      <Me />
-    </>
   );
 }
