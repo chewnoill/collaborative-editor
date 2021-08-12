634ec9051e1b060b3600c316dab422581651af3e
tucker-weed <tuckerjweed@gmail.com>
2021-08-12 13:54:39 -0400

    [refactor] remove editor components from index

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,27 +1,32 @@
 import styled from "@emotion/styled";
 import Me from "components/me";
-import DocumentSelect from "components/select-document";
-import DocumentCreate from "components/create-document";
 import React from "react";
-import dynamic from "next/dynamic";
-
-const Editor = dynamic(() => import("../components/editor"), { ssr: false });
+import { useSelector } from "react-redux";
+import { selectUser } from "../ducks/appState/user";
 
 const PageLayout = styled.div`
   display: flex;
   flex-direction: column;
+  background-color: #f7f4d4;
+`;
+
+const Link = styled.a`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-align: center;
 `;
 
 export default function Index() {
+  const me = useSelector(selectUser);
   return (
-    <PageLayout>
-      <Me />
-      <a href="/login">Login</a>
-      <a href="/create-user">Create Account</a>
-      <a href="/update-password">Update Password</a>
-      <DocumentCreate />
-      <DocumentSelect />
-      <Editor />
-    </PageLayout>
+    <div>
+      <PageLayout>
+        <Me />
+        <Link href="/login">Login</Link>
+        <Link href="/create-user">Create Account</Link>
+        <Link href="/update-password">Update Password</Link>
+        {me ? <Link href="/editor">Documents</Link> : null}
+      </PageLayout>
+    </div>
   );
 }
