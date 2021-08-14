872376c8360c0ddf542642b3289c38ea1a6cb2a3
Will Cohen <chewnoill@gmail.com>
2021-08-14 10:22:50 -0400

    [terraform] public storage bucket

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -17,6 +17,12 @@ resource "google_storage_bucket" "static-site" {
   }
 }
 
+resource "google_storage_bucket_iam_member" "public" {
+  bucket = google_storage_bucket.static-site.name
+  role = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
 resource "random_uuid" "private_bucket" { }
 
 resource "google_storage_bucket" "private_bucket" {
