deb57473bbf38bb79c70a48c0d7d58bfe2e3eb3b
Will Cohen <chewnoill@gmail.com>
2021-10-02 18:19:38 -0400

    [chore] update documents

--- packages/service/src/resolvers/documents.ts
+++ packages/service/src/resolvers/documents.ts
@@ -1,5 +1,5 @@
 import { makeExtendSchemaPlugin, gql } from "graphile-utils";
-import { createDocument } from "../utils/documents";
+import { createDocument, updateDocumentMeta } from "../utils/documents";
 
 const DocumentMutations = makeExtendSchemaPlugin((build) => {
   return {
@@ -7,12 +7,26 @@ const DocumentMutations = makeExtendSchemaPlugin((build) => {
       extend type Mutation {
         # Individual record
         createDoc: Document
+        updateDocument(id: UUID!, update: DocumentUpdateInput!): Document
+      }
+      input DocumentUpdateInput {
+        isPublic: Boolean!
       }
     `,
     resolvers: {
       Mutation: {
         createDoc(_, __, { pgClient }) {
           return createDocument(pgClient);
+        },
+        async updateDocument(_, { id, update }, { pgClient }) {
+          const meta = await updateDocumentMeta(id, update).run(pgClient);
+          if (meta.length === 0) return null;
+          return {
+            ...meta[0],
+            isPublic: meta[0].is_public,
+            latestUpdateTime: meta[0].latest_update_time,
+            creatorId: meta[0].creator_id,
+          };
         },
       },
     },
