2eb556abbb167d620b5eabf077a92fe335c12147
Will Cohen <chewnoill@gmail.com>
2022-05-07 11:41:09 -0400

    [feat] db env guard

--- packages/service/src/env.ts
+++ packages/service/src/env.ts
@@ -1,3 +1,5 @@
+import url from "whatwg-url";
+
 export const NODE_ENV = process.env.NODE_ENV || "production";
 export const HOST = process.env.HOST || "0.0.0.0";
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 6001;
@@ -5,3 +7,9 @@ export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 6001;
 const LOCALHOST_DB_URL =
   "postgres://postgres:password@127.0.0.1:5432/postgres?sslmode=disable";
 export const DATABASE_URL = process.env.DATABASE_URL || LOCALHOST_DB_URL;
+
+try {
+  url.parseURL(DATABASE_URL)
+} catch {
+  throw `Invalid DATABASE_URL config`
+}
