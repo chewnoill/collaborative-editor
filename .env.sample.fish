a8c68037dfca054bf89a8151faf42d6eddeb8509
Will Cohen <chewnoill@gmail.com>
2022-05-18 09:47:23 -0400

    [chore] add redis url

--- .env.sample.fish
+++ .env.sample.fish
@@ -1,4 +1,5 @@
 set -x DATABASE_URL "postgres://postgres:password@127.0.0.1:5432/postgres?sslmode=disable"
+set -x REDIS_URL "redis://default:1234@localhost:6379"
 set -x PORT "8080"
 set -x NEXT_PUBLIC_SIGNAL_URL "ws://localhost:8080/ws/signal"
 set -x NEXT_PUBLIC_PROVIDER_URL "ws://localhost:8080/ws/provider"
