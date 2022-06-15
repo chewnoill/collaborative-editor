c8c432e4bf6956a19d31ada08f6c54d5b37b57e5
Will Cohen <chewnoill@gmail.com>
2022-06-14 22:26:32 -0800

    [chore] add auth session to websockets

--- packages/service/src/service.ts
+++ packages/service/src/service.ts
@@ -4,6 +4,8 @@ import setupSignalingConnection from "./utils/signaling-connection";
 import setupProviderConnection from "./utils/provider-connection";
 import { HOST, PORT } from "./env";
 import ws from "ws";
+import Session from "./session";
+import passport from "./utils/passport";
 
 http.createServer(app);
 
@@ -16,19 +18,27 @@ const wsServer = new ws.Server({ noServer: true });
 wsServer.on("connection-signaling", setupSignalingConnection);
 wsServer.on("connection-provider", setupProviderConnection);
 
+const wrapMiddleware = (middleware) => (request, next) =>
+  middleware(request, {}, next);
+
 server.on("upgrade", (request, socket, head) => {
   // only handle upgrade if path matches
   if (
-    !request.url.startsWith("/ws/provider") &&
-    !request.url.startsWith("/ws/signal")
+    request.url.startsWith("/ws/provider") ||
+    request.url.startsWith("/ws/signal")
   ) {
-    return;
+    wrapMiddleware(Session)(request, () => {
+      wrapMiddleware(passport.initialize())(request, () =>
+        wrapMiddleware(passport.session())(request, () => {
+          wsServer.handleUpgrade(request, socket, head, (socket) => {
+            if (request.url.startsWith("/ws/provider")) {
+              wsServer.emit("connection-provider", socket, request);
+            } else if (request.url.startsWith("/ws/signal")) {
+              wsServer.emit("connection-signaling", socket, request);
+            }
+          });
+        })
+      );
+    });
   }
-  wsServer.handleUpgrade(request, socket, head, (socket) => {
-    if (request.url.startsWith("/ws/provider")) {
-      wsServer.emit("connection-provider", socket, request);
-    } else if (request.url.startsWith("/ws/signal")) {
-      wsServer.emit("connection-signaling", socket, request);
-    }
-  });
 });
