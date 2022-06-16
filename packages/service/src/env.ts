caa886be2279415254e7b3553899494080e775fa
Will Cohen <chewnoill@gmail.com>
2022-06-16 17:19:34 -0400

    [chore] remove default DATABASE_URL

--- packages/service/src/env.ts
+++ packages/service/src/env.ts
@@ -4,9 +4,7 @@ export const NODE_ENV = process.env.NODE_ENV || "production";
 export const HOST = process.env.HOST || "0.0.0.0";
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 6001;
 
-const LOCALHOST_DB_URL =
-  "postgres://postgres:password@127.0.0.1:5432/postgres?sslmode=disable";
-export const DATABASE_URL = process.env.DATABASE_URL || LOCALHOST_DB_URL;
+export const DATABASE_URL = process.env.DATABASE_URL;
 
 try {
   url.parseURL(DATABASE_URL);
