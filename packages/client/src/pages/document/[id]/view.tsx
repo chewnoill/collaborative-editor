ebd9c73d47aeaff8807c23d94f31d880279e9128
Will Cohen <chewnoill@gmail.com>
2022-06-16 17:16:27 -0400

    [feat] document preview

---
+++ packages/client/src/pages/document/[id]/view.tsx
@@ -0,0 +1,24 @@
+import DocumentMenu from "components/document-menu";
+import AppLayout from "layout/app";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import React from "react";
+
+const PreviewDoc = dynamic<any>(
+  () => import("components/document-render-mdx"),
+  {
+    ssr: false,
+  }
+);
+
+export default function Document() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  if (!id) return null;
+  return (
+    <AppLayout>
+      <PreviewDoc id={id} />
+    </AppLayout>
+  );
+}
