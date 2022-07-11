3927ea8a56f4b6a2767265131f7f3b31d0857ab4
wogara <weogara@gmail.com>
2022-07-11 12:54:40 -0400

    [feat] adding document history worker

--- packages/service/src/worker/index.ts
+++ packages/service/src/worker/index.ts
@@ -1,6 +1,10 @@
 import updateDocumentWorker from "./update-document-job";
+import updateDocumentHistoryWorker from "./update-document-history-job";
 
-export default [updateDocumentWorker].reduce((acc, obj) => {
-  acc[obj.key] = obj;
-  return acc;
-}, {});
+export default [updateDocumentWorker, updateDocumentHistoryWorker].reduce(
+  (acc, obj) => {
+    acc[obj.key] = obj;
+    return acc;
+  },
+  {}
+);
