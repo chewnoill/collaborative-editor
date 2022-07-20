e5f9e8cb14219708e464fbc7eb5e23bdd464499b
Will Cohen <chewnoill@gmail.com>
2022-07-20 09:09:41 -0400

    [refactor] call background jobs immediatly
    no need to wait for a schedule

--- packages/service/src/worker/update-document-history-job.ts
+++ packages/service/src/worker/update-document-history-job.ts
@@ -1,38 +1,22 @@
-import * as Y from "yjs";
-import { pool, db } from "../db";
-import gitDiff from "git-diff";
-import { queue } from "../mq";
-import {
-  fetchDocument,
-  selectAllUpdatedDocuments,
-  updateDocumentContent,
-} from "../utils/documents";
-
+import logger from "../logger";
 import { replaceDocumentHistory } from "../utils/document-history";
 
 const config = {
   key: "update-document-history",
-  run: async function updateDocumentHistory(job) {
-    const updatableDocuments = await selectAllUpdatedDocuments();
-    if (updatableDocuments.length === 0) {
-      console.log("nothing to update in history job");
+  run: async function updateDocumentHistory(params: {
+    data: { document_id: string };
+  }) {
+    if (!params?.data?.document_id) {
+      logger({
+        level: "warn",
+        service: "update-document-history",
+        message: "invalid params",
+        body: params,
+      });
       return;
     }
-    await Promise.all(updatableDocuments.map(replaceDocumentHistory));
+    await replaceDocumentHistory({ id: params.data.document_id });
   },
 };
 
 export default config;
-
-async function setupRepeatableJobs() {
-  await queue.add(
-    config.key,
-    {},
-    {
-      repeat: {
-        cron: "*/1 * * * *",
-      },
-    }
-  );
-}
-setupRepeatableJobs();
