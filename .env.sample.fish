fcb64182178857d1292038fa57bf5357a3b8b3ec
Will Cohen <chewnoill@gmail.com>
2022-08-06 10:58:29 -0400

    [refactor] move terraform app into a subfolder

--- .env.sample.fish
+++ .env.sample.fish
@@ -3,7 +3,7 @@ set -x REDIS_URL "redis://default:1234@localhost:6379"
 set -x PORT "8080"
 set -x NEXT_PUBLIC_SIGNAL_URL "ws://localhost:8080/ws/signal"
 set -x NEXT_PUBLIC_PROVIDER_URL "ws://localhost:8080/ws/provider"
-set -x GOOGLE_APPLICATION_CREDENTIALS ../google_creds.json
+set -x GOOGLE_APPLICATION_CREDENTIALS ../../google_creds.json
 set -x GCS_CREDS_FILE ask-a-teammate
 set -x GCS_BUCKET_NAME ask-a-teammate
 
