e9d260d3c216ebd01fd3553b6c8f98d78c9ab55e
Will Cohen <chewnoill@gmail.com>
2022-08-20 09:30:43 -0400

    [feat] namespace schema p4 schema
    add db namespaces to the schema

--- packages/service/db/schema.sql
+++ packages/service/db/schema.sql
@@ -10,36 +10,144 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
+-- Name: app; Type: SCHEMA; Schema: -; Owner: -
 --
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
-
-
---
--- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+CREATE SCHEMA app;
 
 
 --
--- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
 --
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+CREATE SCHEMA public;
 
 
 --
--- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: -
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
 --
 
-COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+COMMENT ON SCHEMA public IS 'standard public schema';
 
 
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: user; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app."user" (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    name text NOT NULL,
+    password text
+);
+
+
+--
+-- Name: TABLE "user"; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON TABLE app."user" IS '
+@name user
+@omit create,update,delete
+
+User Table
+
+The base entity for the application.';
+
+
+--
+-- Name: COLUMN "user".password; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON COLUMN app."user".password IS '
+@name password
+@omit
+
+Password
+
+hashed, still sensitive, do not expose to GQL
+';
+
+
+--
+-- Name: create_user(text, text); Type: FUNCTION; Schema: app; Owner: -
+--
+
+CREATE FUNCTION app.create_user(name text, password text) RETURNS app."user"
+    LANGUAGE sql STRICT
+    AS $$
+  INSERT INTO app.user
+    (name, password)
+    VALUES (name, crypt(password, gen_salt('bf')))
+    RETURNING *;
+$$;
+
+
+--
+-- Name: FUNCTION create_user(name text, password text); Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON FUNCTION app.create_user(name text, password text) IS 'Create User
+
+Creates a user with a given password. A random salt is
+generated, and applied for each password when it is first
+created.
+';
+
+
+--
+-- Name: user_document; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app.user_document (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    document_id uuid,
+    user_id uuid
+);
+
+
+--
+-- Name: TABLE user_document; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON TABLE app.user_document IS '
+@name user_document
+@omit create,update,delete
+
+# User Documents
+
+Manages access control relationship to documents for users
+
+';
+
+
+--
+-- Name: invite_user_to_document(text, uuid); Type: FUNCTION; Schema: app; Owner: -
+--
+
+CREATE FUNCTION app.invite_user_to_document(name text, document_id uuid) RETURNS app.user_document
+    LANGUAGE sql STRICT
+    AS $$
+  INSERT INTO app.user_document
+    (user_id, document_id)
+    VALUES ((SELECT id FROM app.user where name=name), document_id)
+    RETURNING *;
+$$;
+
+
+--
+-- Name: me(); Type: FUNCTION; Schema: app; Owner: -
+--
+
+CREATE FUNCTION app.me() RETURNS app."user"
+    LANGUAGE sql STABLE
+    AS $$
+  SELECT * FROM app.user WHERE id = current_user_id()
+$$;
+
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: -
@@ -96,6 +204,19 @@ $$;
 
 
 --
+-- Name: FUNCTION current_user_id(); Type: COMMENT; Schema: public; Owner: -
+--
+
+COMMENT ON FUNCTION public.current_user_id() IS 'current_user_id
+
+Gets the current user_id from the session settings. Calling client
+must set this setting to enable row level security.
+
+@omit
+';
+
+
+--
 -- Name: user_document; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -141,6 +262,198 @@ $$;
 
 
 --
+-- Name: access_token; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app.access_token (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    user_id uuid NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: data_upload; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app.data_upload (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    file_name text NOT NULL,
+    owner_id uuid NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL,
+    size integer NOT NULL,
+    mime_type text NOT NULL
+);
+
+
+--
+-- Name: TABLE data_upload; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON TABLE app.data_upload IS '
+@name data_upload
+@omit create,update,delete
+
+Data Uploads
+
+This is a table with external keys to links on
+an external system (GCS storage bucket)
+
+';
+
+
+--
+-- Name: document; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app.document (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    creator_id uuid NOT NULL,
+    is_public boolean DEFAULT false,
+    name text DEFAULT ''::text NOT NULL,
+    origin bytea NOT NULL,
+    value text NOT NULL,
+    web_rtc_key text NOT NULL,
+    latest_update_time timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: TABLE document; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON TABLE app.document IS '
+@name document
+@omit create,update,delete
+
+# Documents
+
+The source of truth for all documents in the system.
+
+';
+
+
+--
+-- Name: COLUMN document.latest_update_time; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON COLUMN app.document.latest_update_time IS '
+@name latest_update_time
+@omit create,update
+
+# Last Update Time
+
+Updates are processed through the document_updates_queue table first.
+This records time of the last update processed.
+
+TODO: this should be a foreign key that points directly to a row in
+the document_updates_queue table.
+
+';
+
+
+--
+-- Name: document_history; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app.document_history (
+    document_id uuid NOT NULL,
+    sequence integer NOT NULL,
+    diff text
+);
+
+
+--
+-- Name: TABLE document_history; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON TABLE app.document_history IS '
+@name document_history
+@omit create,update,delete
+
+# Document History
+
+';
+
+
+--
+-- Name: document_history_sequence_seq; Type: SEQUENCE; Schema: app; Owner: -
+--
+
+CREATE SEQUENCE app.document_history_sequence_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: document_history_sequence_seq; Type: SEQUENCE OWNED BY; Schema: app; Owner: -
+--
+
+ALTER SEQUENCE app.document_history_sequence_seq OWNED BY app.document_history.sequence;
+
+
+--
+-- Name: document_update_document_history; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app.document_update_document_history (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    document_update_id uuid NOT NULL,
+    document_id uuid NOT NULL,
+    sequence integer NOT NULL
+);
+
+
+--
+-- Name: TABLE document_update_document_history; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON TABLE app.document_update_document_history IS '
+@name document_update_document_history
+@omit
+
+join table between Document History and Document Update,
+doesnt need to be exposed to gql endpoint.
+
+';
+
+
+--
+-- Name: document_updates_queue; Type: TABLE; Schema: app; Owner: -
+--
+
+CREATE TABLE app.document_updates_queue (
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+    user_id uuid,
+    document_id uuid NOT NULL,
+    document_update bytea NOT NULL,
+    created_at timestamp without time zone DEFAULT now() NOT NULL
+);
+
+
+--
+-- Name: TABLE document_updates_queue; Type: COMMENT; Schema: app; Owner: -
+--
+
+COMMENT ON TABLE app.document_updates_queue IS '
+@name document_updates_queue
+@omit
+
+# Document Update Queue
+
+Updates to the main document CRDT are persisted here first,
+and are eventually processed into the main Document table.
+
+Is not exposed through GQL endpoints.
+
+';
+
+
+--
 -- Name: access_token; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -155,7 +468,12 @@ CREATE TABLE public.access_token (
 -- Name: TABLE access_token; Type: COMMENT; Schema: public; Owner: -
 --
 
-COMMENT ON TABLE public.access_token IS ' ';
+COMMENT ON TABLE public.access_token IS '
+
+Access Tokens
+
+Used to authenticate user on API endpoints. If compromised, these can be revoked.
+';
 
 
 --
@@ -320,10 +638,97 @@ CREATE TABLE public.session (
 
 
 --
+-- Name: document_history sequence; Type: DEFAULT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_history ALTER COLUMN sequence SET DEFAULT nextval('app.document_history_sequence_seq'::regclass);
+
+
+--
 -- Name: document_history sequence; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.document_history ALTER COLUMN sequence SET DEFAULT nextval('public.document_history_sequence_seq'::regclass);
+
+
+--
+-- Name: access_token access_token_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.access_token
+    ADD CONSTRAINT access_token_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: data_upload data_upload_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.data_upload
+    ADD CONSTRAINT data_upload_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: document_history document_history_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_history
+    ADD CONSTRAINT document_history_pkey PRIMARY KEY (document_id, sequence);
+
+
+--
+-- Name: document document_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document
+    ADD CONSTRAINT document_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: document_update_document_history document_update_document_history_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_update_document_history
+    ADD CONSTRAINT document_update_document_history_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: document_updates_queue document_updates_queue_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_updates_queue
+    ADD CONSTRAINT document_updates_queue_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: user_document user_document_document_id_user_id_key; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.user_document
+    ADD CONSTRAINT user_document_document_id_user_id_key UNIQUE (document_id, user_id);
+
+
+--
+-- Name: user_document user_document_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.user_document
+    ADD CONSTRAINT user_document_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: user user_name_key; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app."user"
+    ADD CONSTRAINT user_name_key UNIQUE (name);
+
+
+--
+-- Name: user user_pkey; Type: CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app."user"
+    ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 
 
 --
@@ -430,6 +835,94 @@ CREATE INDEX "IDX_session_expire" ON public.session USING btree (expire);
 
 
 --
+-- Name: access_token access_token_user_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.access_token
+    ADD CONSTRAINT access_token_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+--
+-- Name: data_upload data_upload_owner_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.data_upload
+    ADD CONSTRAINT data_upload_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES app."user"(id) ON DELETE CASCADE;
+
+
+--
+-- Name: document document_creator_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document
+    ADD CONSTRAINT document_creator_id_fkey FOREIGN KEY (creator_id) REFERENCES app."user"(id) ON DELETE CASCADE;
+
+
+--
+-- Name: document_history document_history_document_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_history
+    ADD CONSTRAINT document_history_document_id_fkey FOREIGN KEY (document_id) REFERENCES app.document(id) ON DELETE CASCADE;
+
+
+--
+-- Name: document_update_document_history document_update_document_history_document_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_update_document_history
+    ADD CONSTRAINT document_update_document_history_document_id_fkey FOREIGN KEY (document_id) REFERENCES app.document(id) ON DELETE CASCADE;
+
+
+--
+-- Name: document_update_document_history document_update_document_history_document_id_sequence_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_update_document_history
+    ADD CONSTRAINT document_update_document_history_document_id_sequence_fkey FOREIGN KEY (document_id, sequence) REFERENCES app.document_history(document_id, sequence) ON DELETE CASCADE;
+
+
+--
+-- Name: document_update_document_history document_update_document_history_document_update_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_update_document_history
+    ADD CONSTRAINT document_update_document_history_document_update_id_fkey FOREIGN KEY (document_update_id) REFERENCES app.document_updates_queue(id) ON DELETE CASCADE;
+
+
+--
+-- Name: document_updates_queue document_updates_queue_document_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_updates_queue
+    ADD CONSTRAINT document_updates_queue_document_id_fkey FOREIGN KEY (document_id) REFERENCES app.document(id) ON DELETE CASCADE;
+
+
+--
+-- Name: document_updates_queue document_updates_queue_user_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.document_updates_queue
+    ADD CONSTRAINT document_updates_queue_user_id_fkey FOREIGN KEY (user_id) REFERENCES app."user"(id) ON DELETE CASCADE;
+
+
+--
+-- Name: user_document user_document_document_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.user_document
+    ADD CONSTRAINT user_document_document_id_fkey FOREIGN KEY (document_id) REFERENCES app.document(id) ON DELETE CASCADE;
+
+
+--
+-- Name: user_document user_document_user_id_fkey; Type: FK CONSTRAINT; Schema: app; Owner: -
+--
+
+ALTER TABLE ONLY app.user_document
+    ADD CONSTRAINT user_document_user_id_fkey FOREIGN KEY (user_id) REFERENCES app."user"(id) ON DELETE CASCADE;
+
+
+--
 -- Name: access_token access_token_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -516,6 +1009,95 @@ ALTER TABLE ONLY public.user_document
 ALTER TABLE ONLY public.user_document
     ADD CONSTRAINT user_document_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
+
+--
+-- Name: data_upload access_data_uploads; Type: POLICY; Schema: app; Owner: -
+--
+
+CREATE POLICY access_data_uploads ON app.data_upload TO postgraphile_user USING ((owner_id = public.current_user_id())) WITH CHECK ((owner_id = public.current_user_id()));
+
+
+--
+-- Name: access_token; Type: ROW SECURITY; Schema: app; Owner: -
+--
+
+ALTER TABLE app.access_token ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: access_token access_token_if_allowed; Type: POLICY; Schema: app; Owner: -
+--
+
+CREATE POLICY access_token_if_allowed ON app.access_token TO postgraphile_user USING ((user_id = public.current_user_id())) WITH CHECK ((user_id = public.current_user_id()));
+
+
+--
+-- Name: document create_document_for_current_user; Type: POLICY; Schema: app; Owner: -
+--
+
+CREATE POLICY create_document_for_current_user ON app.document FOR INSERT TO postgraphile_user WITH CHECK ((creator_id = public.current_user_id()));
+
+
+--
+-- Name: data_upload; Type: ROW SECURITY; Schema: app; Owner: -
+--
+
+ALTER TABLE app.data_upload ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: document; Type: ROW SECURITY; Schema: app; Owner: -
+--
+
+ALTER TABLE app.document ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: document_history; Type: ROW SECURITY; Schema: app; Owner: -
+--
+
+ALTER TABLE app.document_history ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: document_updates_queue document_updates_queue_if_allowed; Type: POLICY; Schema: app; Owner: -
+--
+
+CREATE POLICY document_updates_queue_if_allowed ON app.document_updates_queue TO postgraphile_user USING ((document_id IN ( SELECT document.id
+   FROM app.document
+  WHERE (document.id = document_updates_queue.document_id)))) WITH CHECK ((document_id IN ( SELECT document.id
+   FROM app.document
+  WHERE (document.id = document_updates_queue.document_id))));
+
+
+--
+-- Name: user_document invite_to_document_if_allowed; Type: POLICY; Schema: app; Owner: -
+--
+
+CREATE POLICY invite_to_document_if_allowed ON app.user_document FOR INSERT TO postgraphile_user WITH CHECK ((document_id IN ( SELECT document.id
+   FROM app.document
+  WHERE (document.creator_id = public.current_user_id()))));
+
+
+--
+-- Name: document_history select_document_history_if_allowed; Type: POLICY; Schema: app; Owner: -
+--
+
+CREATE POLICY select_document_history_if_allowed ON app.document_history FOR SELECT TO postgraphile_user USING ((document_id IN ( SELECT document.id
+   FROM app.document
+  WHERE (document.id = document_history.document_id))));
+
+
+--
+-- Name: document select_document_if_allowed; Type: POLICY; Schema: app; Owner: -
+--
+
+CREATE POLICY select_document_if_allowed ON app.document FOR SELECT TO postgraphile_user USING (((creator_id = public.current_user_id()) OR (is_public = true) OR (id IN ( SELECT user_document.document_id
+   FROM app.user_document
+  WHERE (user_document.user_id = public.current_user_id())))));
+
+
+--
+-- Name: user_document; Type: ROW SECURITY; Schema: app; Owner: -
+--
+
+ALTER TABLE app.user_document ENABLE ROW LEVEL SECURITY;
 
 --
 -- Name: data_upload access_data_uploads; Type: POLICY; Schema: public; Owner: -
@@ -649,4 +1231,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20220717194812'),
     ('20220725215639'),
     ('20220728132102'),
-    ('20220813173507');
+    ('20220813173507'),
+    ('20220817220159');
