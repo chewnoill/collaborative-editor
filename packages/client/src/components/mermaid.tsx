e37ba1204224133348c6da5e0d56180811e6122f
Will Cohen <chewnoill@gmail.com>
2022-09-22 09:49:26 -0400

    [fix] add unique id to mermaid diagrams

--- packages/client/src/components/mermaid.tsx
+++ packages/client/src/components/mermaid.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import mermaid from "mermaid";
 
+let mermaidId = 0;
+
 const Mermaid = ({ name, children: code }) => {
   const [html, setHtml] = React.useState("");
   React.useEffect(() => {
-    mermaid.mermaidAPI.render(name, code, (html) => setHtml(html));
+    mermaidId += 1;
+    mermaid.mermaidAPI.render(`${name}-${mermaidId}`, code, (html) => setHtml(html));
   }, []);
   return (
     <div className="mermaid" dangerouslySetInnerHTML={{ __html: html }}></div>
