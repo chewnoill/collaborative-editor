cd8cf5a650b00e834fdb4e1ff5a9c43d7b480582
Will Cohen <chewnoill@gmail.com>
2022-10-12 12:11:59 -0400

    [chore] pass creds a different way

--- docker/service/start-app.sh
+++ docker/service/start-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo $GCS_PRIVATE_KEY > gcs_user_content.json
+echo $GCS_PRIVATE_KEY | base64decode -d > gcs_user_content.json
 export GCS_CREDS_FILE="/workspace/gcs_user_content.json"
 
 cd packages/service
