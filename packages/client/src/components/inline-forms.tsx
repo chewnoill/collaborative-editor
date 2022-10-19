e189187475c797b818fdf79c4e6ca6daf1578e44
Will Cohen <chewnoill@gmail.com>
2022-10-18 23:16:47 -0400

    [chore] collapse form when not in use

--- packages/client/src/components/inline-forms.tsx
+++ packages/client/src/components/inline-forms.tsx
@@ -1,23 +1,36 @@
 import styled from "@emotion/styled";
+import { IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import DocumentMetaData from "./document-meta-data";
-import { DocumentName } from "./document-name";
 import SelectTags from "./select-tags";
+import MenuIcon from "@mui/icons-material/Menu";
 
-const List = styled.div`
+const List = styled.div(
+  ({ open }: { open }) => `
+  transition: height 0.1s;
+  height: ${open ? 166 : 0}px;
+  width: 100%;
+
   display: flex;
+  overflow: hidden;
   flex-direction: column;
   & > * {
     margin-bottom: 10px;
   }
-`;
+`);
 
 export default function InlineForm({ document_id }) {
+  const [open, setOpen] = React.useState(false);
   return (
-    <List>
-      <DocumentMetaData document_id={document_id} />
-      <SelectTags document_id={document_id} />
-    </List>
+    <Box sx={{display: "flex",flexDirection:"column", alignItems: "start"}}>
+      <List open={open}>
+        <DocumentMetaData document_id={document_id} />
+        <SelectTags document_id={document_id} />
+      </List>
+      <IconButton onClick={() => setOpen(!open)}>
+        <MenuIcon />
+      </IconButton>
+    </Box>
   );
 }
